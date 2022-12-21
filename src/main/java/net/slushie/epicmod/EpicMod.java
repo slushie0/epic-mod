7786db4df8bef0f4e91c0a1bb62455e904c119f8
Slushie <57730247+slushie0@users.noreply.github.com>
2022-12-20 19:27:57 -0800

    hi

---
+++ src/main/java/net/slushie/epicmod/EpicMod.java
@@ -0,0 +1,14 @@
+package net.slushie.epicmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EpicMod implements ModInitializer {
+	public static final String MOD_ID = "epicmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/slushie/epicmod/EpicModClient.java
@@ -0,0 +1,10 @@
+package net.slushie.epicmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EpicModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
